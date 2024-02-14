35381d365d1b4219e32e0cf5df0ccc35c98450f7
Shishirsingh Rathore <srathore@flexschema.com>
2024-02-14 10:24:03 +0530

    Add project files.

---
+++ PizoneDotNetTask/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿
+using PizoneDotNetTask.Model;
+
+
+
+namespace PizoneDotNetTask.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<SchoolModel> Schools { get; set; }
+        public DbSet<ClassModel> Classes { get; set; }
+        public DbSet<StudentsModel> Students { get; set; }
+
+    }
+}
+
+
+
---
+++ PizoneDotNetTask/Model/SchoolModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PizoneDotNetTask.Model
+{
+    public class SchoolModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int NoOfClass { get; set; }
+    }
+}
